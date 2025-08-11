853348f310bf44346ebc6be83d8348915b752fc3
worktrbz-ship-it <worktrbz@gmail.com>
2025-08-11 11:57:45 +0300

    gazel

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 500,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
